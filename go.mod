d5bd9b963bec3d654d5e652f2c575784c0a7d499
Chris Tompkinson <ctompkinson@gmail.com>
2019-05-18 21:22:01 +0100

    v1

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/spaceapegames/playlist-generator
 go 1.12
 
 require (
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/tidwall/gjson v1.2.1 // indirect
 	github.com/tidwall/match v1.0.1 // indirect
 	github.com/tidwall/pretty v0.0.0-20190325153808-1166b9ac2b65 // indirect
-	github.com/zmb3/spotify v0.0.0-20190210152806-94cbe6dc5cc2 // indirect
-	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a // indirect
+	github.com/zmb3/spotify v0.0.0-20190210152806-94cbe6dc5cc2
+	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a
+	golang.org/x/sys v0.0.0-20190516110030-61b9204099cb // indirect
 )
--- main.go
+++ main.go
@@ -1,38 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/zmb3/spotify"
-	"golang.org/x/oauth2"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2"
 )
 
 var (
-	authConfig spotify.Authenticator
+	authConfig       spotify.Authenticator
 	oauthStateString = ""
 )
 
+type statusResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
+
 	http.HandleFunc("/", handleMain)
 	http.HandleFunc("/login", handleSpotifyLogin)
 	http.HandleFunc("/callback", handleSpotifyCallback)
 	http.HandleFunc("/generate_playlist", handleGenerateFollowersPlaylist)
-	fmt.Println("Listening on 0.0.0.0:7777")
-	http.ListenAndServe("0.0.0.0:7777", nil)
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+
+	fmt.Println("Listening on 0.0.0.0:80")
+	http.ListenAndServe("0.0.0.0:80", nil)
 }
 
 func init() {
 	oauthStateString = generateRandomString(10)
-	clientId := "f0f0b67da33f4f91905518aff025da5e"
-	clientSecret := "b2a1683f60ca435ea536b0144100ff89"
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	redirectUri := os.Getenv("REDIRECT_URI")
 	scopes := "user-read-private user-read-email user-follow-read playlist-read-private playlist-modify-private playlist-modify-public"
-	authConfig = spotify.NewAuthenticator("http://localhost:7777/callback", scopes)
-	authConfig.SetAuthInfo(clientId, clientSecret)
+	authConfig = spotify.NewAuthenticator(redirectUri, scopes)
+	authConfig.SetAuthInfo(clientID, clientSecret)
 }
+
+// Http handlers
 
 func handleMain(w http.ResponseWriter, r *http.Request) {
 	_, err := r.Cookie("access_token")
@@ -51,68 +66,101 @@ func handleSpotifyLogin(w http.ResponseWriter, r *http.Request) {
 
 func handleSpotifyCallback(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("state") != oauthStateString {
-		fmt.Fprintf(w, "oauth state string does not match")
+		logrus.Info("oauth state string does not match")
+		sendStatusResponse(
+			statusResponse{"error", "oauth state string does not match"},
+			http.StatusUnauthorized,
+			w,
+		)
+		return
 	}
 
 	token, err := authConfig.Exchange(r.FormValue("code"))
 	if err != nil {
-		fmt.Fprintf(w, "code exchange failed: %s", err)
+		logrus.Errorf("code exchange failed: %s", err)
+		sendStatusResponse(
+			statusResponse{"error", "code exchange failed"},
+			http.StatusUnauthorized,
+			w,
+		)
+		return
 	}
 
 	http.SetCookie(w, &http.Cookie{
-		Name:       "access_token",
-		Value:      token.AccessToken,
-		Expires:    time.Now().Add(time.Hour * time.Duration(1)),
+		Name:    "access_token",
+		Value:   token.AccessToken,
+		Expires: time.Now().Add(time.Hour * time.Duration(1)),
 	})
 
-	http.ServeFile(w, r, "./public/home.html")
+	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 }
 
 func handleGenerateFollowersPlaylist(w http.ResponseWriter, r *http.Request) {
+	reqValues, err := parseJsonFormResponse(r)
+	if err != nil {
+		logrus.Errorf("failed to parse form: %s", err)
+		sendStatusResponse(
+			statusResponse{"error", "failed to parse form"},
+			http.StatusInternalServerError,
+			w,
+		)
+		return
+	}
+
+	maxTracks, err := strconv.ParseInt(reqValues["max_tracks"], 10, 64)
+	if err != nil {
+		logrus.Errorf("failed to parse max tracks: %s", err)
+		sendStatusResponse(
+			statusResponse{"error", "failed to parse max tracks"},
+			http.StatusInternalServerError,
+			w,
+		)
+		return
+	}
+
 	accessToken, err := r.Cookie("access_token")
 	if err != nil || accessToken == nil || accessToken.Value == "" {
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
-
 	client := authConfig.NewClient(&oauth2.Token{AccessToken: accessToken.Value})
 	client.AutoRetry = true
 
 	user, err := client.CurrentUser()
 	if err != nil {
-		fmt.Fprintf(w, "failed to get current user: %s", err)
+		logrus.Errorf("failed to get current user: %s", err)
+		sendStatusResponse(
+			statusResponse{"error", "failed to get current user"},
+			http.StatusInternalServerError,
+			w,
+		)
 		return
 	}
 
-	var followedArtists []spotify.FullArtist
-	after := 0
-	for len(followedArtists) < 10000 {
-
-		f, err := client.CurrentUsersFollowedArtistsOpt(50, strconv.Itoa(after))
-		if err != nil {
-			fmt.Fprintf(w, "failed to get followed arists: %s", err)
-			return
-		}
-
-		followedArtists = append(followedArtists, f.Artists...)
-		after = after + 50
-		if len(f.Artists) < 50 {
-			break
-		}
+	followedArtists, err := getFollowedArtists(client)
+	if err != nil {
+		logrus.Errorf("failed to get followed artists: %s", err)
+		sendStatusResponse(
+			statusResponse{"error", "failed to get followed artists"},
+			http.StatusInternalServerError,
+			w,
+		)
+		return
 	}
 
-	var playlistTracks []spotify.FullTrack
-	for _, artist := range followedArtists {
-		tracks, err := client.GetArtistsTopTracks(artist.ID, "from_token")
-		if err != nil {
-			fmt.Fprintf(w, "failed to get artists top tracks: %s", err)
-			return
-		}
-		playlistTracks = append(playlistTracks, tracks...)
+	playlistTracks, err := getTopTracksForArists(followedArtists, maxTracks, client)
+	if err != nil {
+		logrus.Errorf("failed to get artists top tracks: %s", err)
+		sendStatusResponse(
+			statusResponse{"error", "failed to get artists top tracks"},
+			http.StatusInternalServerError,
+			w,
+		)
+		return
 	}
 
-	playlistName := "foo"
-	playlistDescription := "bar"
+	playlistName := reqValues["playlist_name"]
+	playlistDescription := reqValues["playlist_description"]
 	playlist, err := client.CreatePlaylistForUser(user.ID, playlistName, playlistDescription, false)
 
 	var trackIDs []spotify.ID
@@ -120,6 +168,31 @@ func handleGenerateFollowersPlaylist(w http.ResponseWriter, r *http.Request) {
 		trackIDs = append(trackIDs, track.ID)
 	}
 
+	if reqValues[""] == "on" {
+		ShuffleTrackIds(trackIDs)
+	}
+
+	err = addTracksToPlaylist(trackIDs, playlist, client)
+	if err != nil {
+		logrus.Errorf("failed to add tracks to playlist: %s", err)
+		sendStatusResponse(
+			statusResponse{"error", "failed to add tracks to playlist"},
+			http.StatusInternalServerError,
+			w,
+		)
+		return
+	}
+
+	sendStatusResponse(
+		statusResponse{"success", "playlist was successfully created"},
+		http.StatusOK,
+		w,
+	)
+}
+
+// Spotify Functions
+
+func addTracksToPlaylist(trackIDs []spotify.ID, playlist *spotify.FullPlaylist, client spotify.Client, ) error {
 	batchSize := 100
 	for i := 0; i < len(trackIDs); i += batchSize {
 		j := i + batchSize
@@ -127,17 +200,72 @@ func handleGenerateFollowersPlaylist(w http.ResponseWriter, r *http.Request) {
 			j = len(trackIDs)
 		}
 
-		_, err = client.AddTracksToPlaylist(playlist.ID, trackIDs[i:j]...)
+		_, err := client.AddTracksToPlaylist(playlist.ID, trackIDs[i:j]...)
 		if err != nil {
-			fmt.Fprintf(w, "failed to add tracks to playlist: %s", err)
-			return
+			return err
 		}
 	}
 
-	fmt.Fprintf(w, "success")
+	return nil
+}
+
+func getTopTracksForArists(artists []spotify.FullArtist, maxTracks int64, client spotify.Client) ([]spotify.FullTrack, error) {
+	var playlistTracks []spotify.FullTrack
+	for _, artist := range artists {
+		tracks, err := client.GetArtistsTopTracks(artist.ID, "from_token")
+		if err != nil {
+			return nil, err
+		}
+
+		if len(tracks) > int(maxTracks) {
+			tracks = tracks[:maxTracks]
+		}
+		playlistTracks = append(playlistTracks, tracks...)
+	}
+	return playlistTracks, nil
+}
+
+func getFollowedArtists(client spotify.Client) ([]spotify.FullArtist, error) {
+	var followedArtists []spotify.FullArtist
+	after := ""
+	for len(followedArtists) < 10000 {
+		f, err := client.CurrentUsersFollowedArtistsOpt(50, after)
+		if err != nil {
+			return nil, err
+		}
+
+		followedArtists = append(followedArtists, f.Artists...)
+		after = f.Cursor.After
+		if len(f.Artists) < 50 {
+			break
+		}
+	}
+	return followedArtists, nil
+}
+
+func parseJsonFormResponse(r *http.Request) (map[string]string, error) {
+	decoder := json.NewDecoder(r.Body)
+	var v []map[string]string
+	err := decoder.Decode(&v)
+	if err != nil {
+		panic(err)
+	}
+
+	reqValues := make(map[string]string)
+	for _, pair := range v {
+		reqValues[pair["name"]] = pair["value"]
+	}
+
+	return reqValues, err
 }
 
 // Helpers
+
+func sendStatusResponse(status statusResponse, responseCode int, w http.ResponseWriter) {
+	b, _ := json.Marshal(status)
+	http.Error(w, string(b), responseCode)
+}
+
 func generateRandomString(n int) string {
 	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	b := make([]rune, n)
@@ -145,4 +273,14 @@ func generateRandomString(n int) string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
+}
+
+func ShuffleTrackIds(vals []spotify.ID) {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for len(vals) > 0 {
+		n := len(vals)
+		randIndex := r.Intn(n)
+		vals[n-1], vals[randIndex] = vals[randIndex], vals[n-1]
+		vals = vals[:n-1]
+	}
 }
